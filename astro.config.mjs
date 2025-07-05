29ccd1fe79d1cdd3fd0e099f0ca1d1358904212b
Xi Ouyang <simonouyang@gmail.com>
2025-07-05 09:35:06 -0700

    ensure it works

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://skylineautorepair.com',
   vite: {
     plugins: [tailwindcss()]
   },
