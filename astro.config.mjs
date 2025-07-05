3d102ea547d255be818e9401639a973aa27145a5
Xi Ouyang <simonouyang@gmail.com>
2025-07-05 10:56:50 -0700

    fix tailwind css issues, always use Astro tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skylineautorepair.com',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx()]
 });
